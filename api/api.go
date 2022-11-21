8864ff9a6d856d5c65b575616446059acf29360e
maru <maru@myyahoo.com>
2022-11-20 22:34:36 -0500

    Split into separate endpoints

--- api/api.go
+++ api/api.go
@@ -12,7 +12,9 @@ import (
 )
 
 func Init() {
-	http.HandleFunc("/", handleRankings)
+	http.HandleFunc("/categories", handleCategories)
+	http.HandleFunc("/page", handlePage)
+	http.HandleFunc("/list", handleList)
 
 	http.Serve(getListener(), nil)
 }
@@ -34,7 +36,29 @@ func getListener() net.Listener {
 	return listener
 }
 
-func handleRankings(w http.ResponseWriter, r *http.Request) {
+func handleCategories(w http.ResponseWriter, r *http.Request) {
+	gameParam, ok := r.URL.Query()["game"]
+	if !ok || len(gameParam) == 0 {
+		http.Error(w, "game not specified", http.StatusBadRequest)
+		return
+	}
+
+	rankingCategories, err := database.GetRankingCategories(gameParam[0])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rankingCategoriesJson, err := json.Marshal(rankingCategories)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(rankingCategoriesJson)
+}
+
+func handlePage(w http.ResponseWriter, r *http.Request) {
 	var uuid string
 
 	token := r.Header.Get("Authorization")
@@ -42,97 +66,74 @@ func handleRankings(w http.ResponseWriter, r *http.Request) {
 		uuid = database.GetPlayerUuidFromToken(token)
 	}
 
+	categoryParam, ok := r.URL.Query()["category"]
+	if !ok || len(categoryParam) == 0 {
+		http.Error(w, "category not specified", http.StatusBadRequest)
+		return
+	}
+
+	subCategoryParam, ok := r.URL.Query()["subCategory"]
+	if !ok || len(subCategoryParam) == 0 {
+		http.Error(w, "subcategory not specified", http.StatusBadRequest)
+		return
+	}
+
+	playerPage := 1
+	if token != "" {
+		var err error
+		playerPage, err = database.GetRankingEntryPage(uuid, categoryParam[0], subCategoryParam[0])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Write([]byte(strconv.Itoa(playerPage)))
+}
+
+func handleList(w http.ResponseWriter, r *http.Request) {
 	gameParam, ok := r.URL.Query()["game"]
 	if !ok || len(gameParam) == 0 {
 		http.Error(w, "game not specified", http.StatusBadRequest)
 		return
 	}
 
-	commandParam, ok := r.URL.Query()["command"]
-	if !ok || len(commandParam) == 0 {
-		http.Error(w, "command not specified", http.StatusBadRequest)
+	categoryParam, ok := r.URL.Query()["category"]
+	if !ok || len(categoryParam) == 0 {
+		http.Error(w, "category not specified", http.StatusBadRequest)
 		return
 	}
 
-	switch commandParam[0] {
-	case "categories":
-		rankingCategories, err := database.GetRankingCategories(gameParam[0])
+	subCategoryParam, ok := r.URL.Query()["subCategory"]
+	if !ok || len(subCategoryParam) == 0 {
+		http.Error(w, "subcategory not specified", http.StatusBadRequest)
+		return
+	}
+
+	var page int
+	pageParam, ok := r.URL.Query()["page"]
+	if !ok || len(pageParam) == 0 {
+		page = 1
+	} else {
+		pageInt, err := strconv.Atoi(pageParam[0])
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		rankingCategoriesJson, err := json.Marshal(rankingCategories)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(rankingCategoriesJson)
-	case "page":
-		categoryParam, ok := r.URL.Query()["category"]
-		if !ok || len(categoryParam) == 0 {
-			http.Error(w, "category not specified", http.StatusBadRequest)
-			return
-		}
-
-		subCategoryParam, ok := r.URL.Query()["subCategory"]
-		if !ok || len(subCategoryParam) == 0 {
-			http.Error(w, "subcategory not specified", http.StatusBadRequest)
-			return
-		}
-
-		playerPage := 1
-		if token != "" {
-			var err error
-			playerPage, err = database.GetRankingEntryPage(uuid, categoryParam[0], subCategoryParam[0])
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-
-		w.Write([]byte(strconv.Itoa(playerPage)))
-	case "list":
-		categoryParam, ok := r.URL.Query()["category"]
-		if !ok || len(categoryParam) == 0 {
-			http.Error(w, "category not specified", http.StatusBadRequest)
-			return
-		}
-
-		subCategoryParam, ok := r.URL.Query()["subCategory"]
-		if !ok || len(subCategoryParam) == 0 {
-			http.Error(w, "subcategory not specified", http.StatusBadRequest)
-			return
-		}
-
-		var page int
-		pageParam, ok := r.URL.Query()["page"]
-		if !ok || len(pageParam) == 0 {
 			page = 1
 		} else {
-			pageInt, err := strconv.Atoi(pageParam[0])
-			if err != nil {
-				page = 1
-			} else {
-				page = pageInt
-			}
+			page = pageInt
 		}
-
-		rankings, err := database.GetRankingsPaged(gameParam[0], categoryParam[0], subCategoryParam[0], page)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		rankingsJson, err := json.Marshal(rankings)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(rankingsJson)
-	default:
-		http.Error(w, "unknown command", http.StatusBadRequest)
 	}
+
+	rankings, err := database.GetRankingsPaged(gameParam[0], categoryParam[0], subCategoryParam[0], page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rankingsJson, err := json.Marshal(rankings)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(rankingsJson)
 }
